a27e34934fa1a1ea6c0754be7fc36cfe5b553953
eamonwong <eamon.wong2000@gmail.com>
2025-03-19 22:14:37 +0000

    Modify app.py
    modify app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask,request,render_template
+from flask import Flask, render_template, request
 import pyshorteners
-app=Flask(__name__)
-
-@app.route('/')
+app = Flask(__name__)
+ 
+@app.route("/", methods=['POST', 'GET'])
 def home():
-    if request.method=='GET':
-        url=request.args('name')
-        s=pyshorteners.Shortener()
-        shorter=s.tinyurl.short(url)
-        return shorter
-
-    return render_template('home.html')
-
-if __name__=='__main__':
-    app.run(debug=True)
+  if request.method=="POST":
+    url_received = request.form["url"]
+    short_url = pyshorteners.Shortener().tinyurl.short(url_received)
+    return render_template("form.html", new_url=short_url, old_url=url_received)
+  else:
+    return render_template('form.html')
+ 
+if __name__ == "__main__":
+ app.run() 
